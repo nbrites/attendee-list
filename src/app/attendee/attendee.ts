2df58d08f3d21ed8a31fae3f2f461abf24eda1a2
Nuno Brites <nuno.brites@totemic.co.uk>
2017-10-06 13:50:08 +0100

    Adds attendee data structure

---
+++ src/app/attendee/attendee.ts
@@ -0,0 +1,11 @@
+export class Attendee {
+  id: string;
+  first_name: string;
+  last_name: string;
+  job_title: string;
+  bio: string;
+  company_name: string;
+  country_id: string;
+  avatar_url: string;
+  industry_id: string;
+}
